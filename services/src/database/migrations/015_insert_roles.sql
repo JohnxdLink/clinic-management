8697c610a84a79aa58d7339e564376367b29a7db
vincent64659 <vincentpechera0909@gmail.com>
2026-09-03 15:40:14 +0800

    feat: backend file

---
+++ services/src/database/migrations/015_insert_roles.sql
@@ -0,0 +1,10 @@
+-- Table        : roles
+-- Defination   : Add super admin for the first role. 
+-- Date         : DD-MM-YYYY
+
+
+INSERT INTO roles (name, description)
+VALUES (
+    'superadmin',
+    'Full system administrator with complete access to all system features and resources.'
+);
---
+++ services/src/database/migrations/016_insert_accounts.sql
@@ -0,0 +1,9 @@
+-- Table        : accounts
+-- Defination   : Add ma'am Eli's account for computer lab attendance. 
+-- Date         : DD-MM-YYYY
+
+INSERT INTO accounts (username, password)
+VALUES (
+    'admin',
+    'paste_the_bcrypt_here'
+);
